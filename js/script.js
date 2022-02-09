fc252ca89ca95499a1eb4e22bf07e879f694a157
jonnebonde <jonnebonde@gmail.com>
2022-02-09 17:36:08 +0100

    enter

--- js/script.js
+++ js/script.js
@@ -46,53 +46,49 @@ const URL = corsUrl + gameUrl;
 
 const apiResult = document.querySelector("#apiResult");
 
+// This loader SVG is not mine. I have copied it from codepen. Link to the creator is located in the Readme file. 
+const loadingContainer = document.querySelector(".loading");
+loadingContainer.innerHTML += `
+    <h2>Your games is loading</h2>
+    <svg viewBox="-2000 -1000 4000 2000">
+    <path id="inf" d="M354-354A500 500 0 1 1 354 354L-354-354A500 500 0 1 0-354 354z"></path>
+    <use xlink:href="#inf" stroke-dasharray="1570 5143" stroke-dashoffset="6713px"></use></svg>`;
+   
+
 setTimeout(() => {
   async function getGame() {
     const responds = await fetch(URL);
 
     const result = await responds.json();
 
-    
-    const gameObject = result.results;
+    let gameObject = result.results;
 
-    const games = await gameObject;
+    gameObject = gameObject.filter(function(id){
+        return id.id !== 388568;
+      }) 
 
-    const gameFilter = games.filter(function (game) {
-      if (!gameObject.rating) {
-        // Game has no rating
-        return false; // Exclude it
-      } /* else if (gameObject.id === (gameObject.id)) {
-        return false; // Exclude it
-      } */ /* else {
-        return true; // Include it
-      } */
-    });
-
-
-    let removeGame = gameObject.name;
-    
-    console.log(gameObject)
     for (var i = 0; i < gameObject.length; i++) {
+      if (i === 8) {
+        break;
+      }
       const gameName = gameObject[i].name;
       const gameRate = gameObject[i].rating;
       const gameTags = gameObject[i].tags.length;
-      const gameImage = gameObject[i].background_image;
+      const gameImage = gameObject[i].background_image; 
 
-      if(i === 7) {
-          break;
-      }
-      
+      loadingContainer.innerHTML = "";
+
       apiResult.innerHTML += `
-  <div class="game-card">
-  <div class="ratio-box">
-  <img src="${gameImage}" class="image" alt="image missing" />
-  </div>
-  <h3> ${gameName} </h3>
-  <p>Rating: ${gameRate} </p>
-  <p>Tags: ${gameTags} </p>
-  </div>
-`;
+          <div class="game-card">
+          <div class="ratio-box">
+          <img src="${gameImage}" class="image" alt="image missing" />
+          </div>
+          <h3 class="game-title"> ${gameName} </h3>
+          <p>Rating: ${gameRate} </p>
+          <p>Tags: ${gameTags} </p>
+          <a class="cta-btn">Read more</a>
+          </div>`;
     }
   }
   getGame();
-}, 000);
+}, 3000);
